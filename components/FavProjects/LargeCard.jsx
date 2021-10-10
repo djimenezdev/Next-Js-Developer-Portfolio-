d764f37824d46ac661888b927f750a0664786539
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-10 10:57:48 -0700

    favorite projects on home

---
+++ components/FavProjects/LargeCard.jsx
@@ -0,0 +1,33 @@
+import { imgBlur } from "@constants/data";
+import Image from "next/image";
+
+const LargeCard = () => {
+  return (
+    <a
+      href="https://airbnb-clone-with-nextjs.vercel.app/"
+      className="w-full block col-span-3 shadow-2xl "
+    >
+      <div className="relative overflow-hidden rounded-md">
+        <div className="image-container max-h-[517px]">
+          <Image
+            src="/images/airbnb-clone.jpg"
+            alt="portfolio"
+            className="portfolio__image transform hover:scale-125 transition duration-2000 ease-out"
+            layout="fill"
+            quality={65}
+            placeholder="blur"
+            blurDataURL={imgBlur}
+          />
+        </div>
+        <h1 className="absolute top-10 left-10 text-white font-bold text-xl bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] rounded-md px-2">
+          Airbnb Clone built with NextJS
+        </h1>
+        <h1 className="absolute bottom-10 left-10 text-white bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] font-bold text-xl rounded-md px-2">
+          01
+        </h1>
+      </div>
+    </a>
+  );
+};
+
+export default LargeCard;
---
+++ components/FavProjects/MediumCard.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+const MediumCard = () => {
+  return (
+    <a
+      href="https://www.marlontherealtor.com/"
+      className="w-full block lg:col-span-2  col-span-3 shadow-2xl "
+    >
+      <div className="relative overflow-hidden rounded-md">
+        {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
+        <div className="image-container max-h-[348px]">
+          <Image
+            src="/images/mdshomes-website.jpg"
+            alt="portfolio"
+            className="portfolio__image transform hover:scale-125 transition duration-2000 ease-out"
+            layout="fill"
+            quality={65}
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAaCAIAAAB6opi9AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAEySURBVFhH1VdLFsIgDAzeyrN7MjfFSaC0lJLqwMLOw8cnH4ZMFjU8X2+JEmOURYCw2DpKGiGGtMW5nQRZMK0LLC0K25DP84gp2+oAK9JYlG7hoKZk1UW+HbPmifIwrz9AyHNCnxYe0Ud6quvCoHDr06rpn0JdztzG6fIi9miTnOowkpZTSpgcaxcWU7iRtBCfxuGVs0C2/HzU1/Etn8ViBOuiJONbfjLq55EiTq3RhnInKSLi0/DZf4k2x9+IWFPjaaGaWtCmqLmKY7hdy7twG+9ntNlIWptM44IdAI7hfi3vlmEreyOAvZaEBuJe7+vUze1LRwtbAueLSHPaY5jWFBYrVCFrgvnVohtrD5JWedYpC5qZlh4/r+VddTQW/+csRQU3StGjbOfFOF/Ea2aXEPkAw5No5T4ThIoAAAAASUVORK5CYII="
+          />
+        </div>
+        <h1 className="absolute top-10 left-10 text-white font-bold text-xl bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] rounded-md px-2">
+          MDS Homes Realtor Website
+        </h1>
+        <h1 className="absolute bottom-10 left-10 text-white bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] font-bold text-xl rounded-md px-2">
+          02
+        </h1>
+      </div>
+    </a>
+  );
+};
+
+export default MediumCard;
