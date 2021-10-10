fece3014711658687d64d5b85fe4bd9dad75885b
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-10 10:10:39 -0700

    pages config

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,33 @@
-import "../styles/globals.css";
+import "@styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config, library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faCircle, faSquareFull } from "@fortawesome/free-solid-svg-icons";
+config.autoAddCss = false;
+library.add(
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+  faGithub,
+  faFacebook,
+  faSquareFull,
+  faCircle
+);
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme="light" attribute="class">
+    <ThemeProvider
+      defaultTheme="light"
+      attribute="class"
+      disableTransitionOnChange={false}
+    >
       <Component {...pageProps} />
     </ThemeProvider>
   );
