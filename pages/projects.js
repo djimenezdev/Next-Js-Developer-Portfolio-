ac9c0c28d3a63ebb88ee618f9c640f34c39f10e1
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-10 10:04:31 -0700

    projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,15 @@
-import React from "react";
-import ContainerBlock from "../components/ContainerBlock";
-import Projects from "../components/Projects";
+import ContainerBlock from "@components/ContainerBlock";
+import Projects from "@components/Projects/Projects";
 
 export default function projects() {
   return (
-    <ContainerBlock title="Projects - Manu Arora">
+    <ContainerBlock
+      title="Projects - Daniel Jimenez"
+      description="Full Stack Web developer who developing websites with passion"
+      type="website"
+      image="/images/projects-screenshot.png"
+      date={new Date()}
+    >
       <Projects />
     </ContainerBlock>
   );
