ec1c2848294c9f27d2c9ec6e6a40bfcad64b577a
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-10 16:31:41 -0700

    added custom tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,23 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: "class",
   theme: {
     extend: {
       transitionDuration: {
         0: "0ms",
         2000: "2000ms",
         4000: "4000ms",
+      },
+      boxShadow: {
+        "shadow-white": "0px 1px 3px -1px  #FFFFFF",
+      },
+      screens: {
+        lgTwo: "1059px",
+      },
+      backgroundImage: {
+        "instagram-gradient":
+          "linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d)",
       },
     },
   },
