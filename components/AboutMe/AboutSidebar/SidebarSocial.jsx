e837928145757a48ad9689f4b718aef7ec205d6b
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-14 00:39:26 -0700

    updated instagram icon mask color

--- components/AboutMe/AboutSidebar/SidebarSocial.jsx
+++ components/AboutMe/AboutSidebar/SidebarSocial.jsx
@@ -23,7 +23,7 @@ const SidebarSocial = ({ title, link, pathInfo }) => {
               icon={["fab", title]}
               size="2x"
               mask="square-full"
-              className="text-[#F1F1F1] dark:text-[#111827]  bg-instagram-gradient"
+              className="text-[#F1F1F1] dark:text-gray-900 bg-instagram-gradient border-0"
             />
           ) : title === "twitter" ? (
             <FontAwesomeIcon
