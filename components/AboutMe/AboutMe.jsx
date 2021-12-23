5223d3a54e32b358c2380c17551bb3c94c36189d
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-12-23 15:49:53 -0800

    removed comments

--- components/AboutMe/AboutMe.jsx
+++ components/AboutMe/AboutMe.jsx
@@ -11,9 +11,6 @@ export default function AboutMe() {
         <div className="pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20">
           {/* update cv for  job opp section */}
           <AboutSideBar user={userData} />
-          {/* Tech area */}
-          {/* change about bio */}
-          {/* change tech images and store images links in array and map through instead of manually add each */}
           <AboutTech user={userData} />
         </div>
       </div>
