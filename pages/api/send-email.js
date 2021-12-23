032fb9123faff3d562e5e9d897aecb0ca3803975
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-12-23 15:49:23 -0800

    replaced nodemailer with sendgrid smtp

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,27 +1,45 @@
-const nodemailer = require("nodemailer");
-
-let transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 587,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const sendGridMail = require("@sendgrid/mail");
+sendGridMail.setApiKey(process.env.EMAIL_PASS);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, email, message } = req.body;
-    let info = await transporter.sendMail({
-      from: process.env.EMAIL_FROM,
-      to: process.env.EMAIL_TO,
-      subject: `${name} has a message for you`,
-      html: `Email:${email}<br/>Message:${message}`,
-    });
-    if (info) {
-      res.json({ res: "success" });
-    } else {
-      res.json({ res: "Not successful" });
-    }
+
+    await sendGridMail
+      .send({
+        from: process.env.EMAIL_FROM,
+        to: process.env.EMAIL_TO,
+        templateId: process.env.EMAIL_ID_TWO,
+        dynamicTemplateData: {
+          contactSubject: `${name} has a message for you`,
+          contactEmail: email,
+          contactMessage: message,
+        },
+      })
+      .then(
+        async () => {
+          await sendGridMail
+            .send({
+              from: process.env.EMAIL_FROM,
+              to: email,
+              replyTo: process.env.EMAIL_TO,
+              templateId: process.env.EMAIL_ID,
+              dynamicTemplateData: {
+                name: name,
+              },
+            })
+            .then(
+              (response) => {
+                res.json({ res: "success" });
+              },
+              (error) => {
+                res.json({ res: "Not successful" });
+              }
+            );
+        },
+        (error) => {
+          res.json({ res: "Not successful" });
+        }
+      );
   }
 }
