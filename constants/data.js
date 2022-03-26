29cfed108e505582cc10ffd8e51195427a34027f
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2022-03-26 01:36:52 -0700

    data update

--- constants/data.js
+++ constants/data.js
@@ -1,62 +1,90 @@
 const userData = {
   githubUsername: "djimenezdev",
   name: "Daniel Jimenez",
-  designation: "Full-Stack Developer",
+  designation: "Full-Stack/Blockchain Developer",
   avatarUrl: "/images/portfolio-img.jpg",
   email: "daniel@djimenezdev.com",
   phone: "+1 661-544-5098",
   address: "Lancaster, California",
   projects: [
     {
-      title: "MDS Homes  Website",
-      link: "https://www.marlontherealtor.com/",
-      imgUrl: "/images/mds-website.png",
+      title: "Opensea Market Clone",
+      link: "https://os-market-clone.vercel.app/",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/opensea-img.png?alt=media&token=eb7ac955-19e6-4066-84a3-db240cf51350",
     },
     {
-      title: "Rising Crytals",
-      link: "https://www.lorinamarie.com",
-      imgUrl: "/images/rising-crystals.png",
+      title: "DJ NFT",
+      link: "https://dj-nft-vite.netlify.app/",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/djnftvite.png?alt=media&token=e7b53287-d93c-4332-a61c-6783f2975fb3",
     },
     {
-      title: "Airbnb Clone ",
-      link: "https://airbnb-clone-with-nextjs.vercel.app/",
-      imgUrl: "/images/airbnb-clone.jpg",
-    },
-    {
-      title: "Uber Clone",
-      link: "https://expo.io/@djimenez0255/projects/uberClone",
-      imgUrl: "/images/uber-clone.jpg",
-    },
-    {
-      title: "Amazon Clone",
-      link: "https://clone-stripe.web.app/",
-      imgUrl: "/images/amazon-clone.jpg",
-    },
-    {
-      title: "Netflix Clone",
-      link: "https://netflix-challenge-clone.web.app/",
-      imgUrl: "/images/netflix-clone.jpg",
+      title: "DJ Coinbase Clone",
+      link: "https://dj-coinbase-clone.vercel.app/",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/Screenshot%202022-03-25%20214528.png?alt=media&token=bcfdcd6e-9f65-4e7d-83fa-0e1e0ea3293e",
     },
     {
       title: "CryptoTracker",
       link: "https://expo.dev/@djimenez0255/CryptoApp",
-      imgUrl: "/images/cryptoTracker-thumbnail.jpg",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/cryptoTracker-thumbnail.jpg?alt=media&token=108e9d32-2114-4179-8471-3c258962765b",
     },
+    {
+      title: "MDS Homes  Website",
+      link: "https://www.marlontherealtor.com/",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/mds-website.png?alt=media&token=a1fcb5fd-32cf-4189-8b72-4ffba5093ecd",
+    },
+    {
+      title: "Rising Crytals",
+      link: "https://www.lorinamarie.com",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/rising-crystals.png?alt=media&token=052589bc-be3d-4237-a874-551425f3397a",
+    },
+    {
+      title: "Airbnb Clone ",
+      link: "https://airbnb-clone-with-nextjs.vercel.app/",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/airbnb-clone.jpg?alt=media&token=bfdd8c48-0fbe-4137-90aa-06e6a5edb0ec",
+    },
+    {
+      title: "Uber Clone",
+      link: "https://expo.io/@djimenez0255/projects/uberClone",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/uber-clone.jpg?alt=media&token=26f25114-ace0-4a3a-8911-3da50cafb19b",
+    },
+    {
+      title: "Amazon Clone",
+      link: "https://clone-stripe.web.app/",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/amazon-clone.jpg?alt=media&token=c7a48761-fe3a-4ccb-adcd-0c438ddeda1f",
+    },
+    {
+      title: "Netflix Clone",
+      link: "https://netflix-challenge-clone.web.app/",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/netflix-clone.jpg?alt=media&token=1351c2af-94a1-47bc-9378-9af4da1f2ce2",
+    },
+
     {
       title: "Signal Clone",
       link: "https://expo.dev/@djimenez0255/signal-clone",
-      imgUrl: "/images/Signal-Clone-Thumbnail.jpg",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/Signal-Clone-Thumbnail.jpg?alt=media&token=93c90dac-63af-4f3a-bde0-afef81e51aa3",
     },
     {
       title: "WP Portfolio",
       link: "https://portfolio.freelancerpro.org/",
-      imgUrl: "/images/wp-portfolio-thumbnail.jpg",
+      imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/wp-portfolio-thumbnail.jpg?alt=media&token=6d2762dc-4762-4ebe-bf6e-5b9bc7cb37ab",
     },
   ],
   about: {
     title: [
       "I'm a ",
-      " California Based Full Stack developer & tutor",
+      " California Based Full Stack/Blockchain developer & tutor",
       " that loves building web applications and mobile apps that bring great value to others.",
       " Always love to take on a challenge!",
     ],
@@ -64,6 +92,7 @@ const userData = {
       `Technology has always been a part of my genes. `,
       `At the age of 15 is when web development in particular clicked with me. I loved being able to type up some html, css & js  to put together amazing websites. But it wasn't until Rafeh Qazi, the founded of Clever Programmer, introduced me into Full Stack Development. From there I was hooked!`,
       `I have been blessed to be one of his success coaches for his program Profit with JavaScript being able to learn from amazing developers and being able to teach that to the amazing students. And I have been able to do so much more to continue my journey as a Full Stack Developer.`,
+      `Taking the skills I've learned as a Full Stack Development, I made the transition into web 3.0 and have had no regrets! This evolution of the internet has provided a new, unique and powerful way to communicate digitally with these powerful blockchains. There is so much to explore and I am so excited to explore more and share that with others!`,
       `I went from just knowing the bare bone basics to building high level applications. I'm excited to bring my knowledge and experience where I go next on my journey:)`,
     ],
   },
@@ -108,19 +137,37 @@ const userData = {
     facebook: "https://www.facebook.com/djimenezdev",
   },
   techStack: {
-    javaScript:
-      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png",
+    thirdweb: "https://avatars.githubusercontent.com/u/79496167?s=200&v=4",
+    moralis: "https://avatars.githubusercontent.com/u/80474746?s=200&v=4",
+    solidity:
+      "https://raw.githubusercontent.com/github/explore/ba9de12f88fd08825c51928e91f1678cb5c94b26/topics/solidity/solidity.png",
+    remix:
+      "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/remix-ide.png?alt=media&token=8c69c88a-21ae-42f0-ac94-ac99a7ac04dd",
+    oz: "https://avatars.githubusercontent.com/u/20820676?s=200&v=4",
     html: "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png",
     css: "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png",
+    bootstrap: "https://avatars.githubusercontent.com/u/2918581?s=200&v=4",
+    tailwind: "https://avatars.githubusercontent.com/u/67109815?s=200&v=4",
     git: "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png",
+    javaScript:
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png",
+    node: "https://avatars.githubusercontent.com/u/9950313?s=200&v=4",
     react:
       "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png",
+    nextJS:
+      "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/nextJS-logo.png?alt=media&token=d3edf8d6-f24d-46fe-847e-bf22a79a2d23",
+    redux: "https://avatars.githubusercontent.com/u/13142323?s=200&v=4",
     firebase:
       "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png",
+    sanityio: "https://avatars.githubusercontent.com/u/17177659?s=200&v=4",
     mongoDB:
       "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png",
-    nextJS: "/images/nextJS-logo.png",
-    story: "/images/story-logo.png",
+    story:
+      "https://firebasestorage.googleapis.com/v0/b/djimenezdev-portfolio-backend.appspot.com/o/story-logo.png?alt=media&token=f0b2ef18-9b4d-47ae-865c-571f3f8578cb",
+    wp: "https://avatars.githubusercontent.com/u/276006?s=200&v=4",
+    woo: "https://avatars.githubusercontent.com/u/473596?s=200&v=4",
+    vite: "https://avatars.githubusercontent.com/u/65625612?s=200&v=4",
+    stripe: "https://avatars.githubusercontent.com/u/856813?s=200&v=4",
   },
 };
 
