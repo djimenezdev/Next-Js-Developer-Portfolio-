a7f7198c88a110dca6e8a780bb0f743b1a4add21
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-05 16:30:53 -0700

    not removed just updated with ts

--- utils/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import routeReducer from '@lib/Slices/routeSlice';
-
-export const store = configureStore({
-  reducer: {
-    route: routeReducer,
-  },
-});
