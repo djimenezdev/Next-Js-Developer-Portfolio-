d7552d083dc27bbfb7fd8d26ece9d3360d302873
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-05 01:56:41 -0700

    contact form page components

---
+++ components/Contact/ContactHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const ContactHeader = () => {
+  return (
+    <header>
+      <h1 className='text-gray-50 font-semibold text-2xl'>
+        Get in touch, let's talk.
+      </h1>
+      <p className='font-light text-base text-gray-200 mt-2'>
+        Fill in the details and I'll get back to you as soon as I can.
+      </p>
+    </header>
+  );
+};
