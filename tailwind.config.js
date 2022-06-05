8a9c4259395f24e7d9e763e0eb479b6cf5c3b41c
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-05 01:55:30 -0700

    updated breakpoint config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,37 @@
-const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
+    screens: {
+      xss: '374px',
+      xs: '425px',
+      ...defaultTheme.screens,
+    },
     extend: {
       transitionDuration: {
-        0: "0ms",
-        2000: "2000ms",
-        4000: "4000ms",
+        0: '0ms',
+        2000: '2000ms',
+        4000: '4000ms',
+      },
+      // use extend to add larger breakpoints
+      screens: {
+        lgTwo: '1059px',
       },
       boxShadow: {
-        "shadow-white": "0px 1px 3px -1px  #FFFFFF",
-      },
-      screens: {
-        xs: "425px",
-        lgTwo: "1059px",
+        'shadow-white': '0px 1px 3px -1px  #FFFFFF',
       },
       backgroundImage: {
-        "instagram-gradient":
-          "linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d)",
+        'instagram-gradient':
+          'linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d)',
       },
     },
   },
   variants: {
     extend: {},
-    display: ["responsive", "group-hover", "group-focus"],
+    display: ['responsive', 'group-hover', 'group-focus'],
   },
 
   plugins: [],
