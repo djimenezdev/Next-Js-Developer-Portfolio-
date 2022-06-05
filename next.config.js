399be3c9af31a6589431c4346cc70d23cc1defbd
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-05 16:59:23 -0700

    next typescript build config update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   images: {
     domains: [
-      "avatars.githubusercontent.com",
-      "raw.githubusercontent.com",
-      "firebasestorage.googleapis.com",
+      'avatars.githubusercontent.com',
+      'raw.githubusercontent.com',
+      'firebasestorage.googleapis.com',
     ],
+  },
+  typescript: {
+    // to skip fontawesome type error. it shows icon perfectly complaining about wanting the string literal
+    ignoreBuildErrors: true,
   },
 };
