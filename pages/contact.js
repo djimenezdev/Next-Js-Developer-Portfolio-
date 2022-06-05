918236b07a780a926d73d8078150263bee95548e
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-05 02:02:43 -0700

    formatted pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,13 @@
-import ContainerBlock from "@components/ContainerBlock";
-import Contact from "@components/Contact";
+import ContainerBlock from '@components/ContainerBlock';
+import Contact from '@components/Contact/Contact';
 
 export default function contact() {
   return (
     <ContainerBlock
-      title="Contact Me - Daniel Jimenez"
+      title='Contact Me - Daniel Jimenez'
       description="Hello my name is Daniel Jimenez! I'm a Full Stack Web dev who developes websites with passion! Contact me for further details on collaborating together"
-      type="website"
-      image="/images/contact-screenshot.png"
+      type='website'
+      image='/images/contact-screenshot.png'
       date={new Date()}
     >
       <Contact />
