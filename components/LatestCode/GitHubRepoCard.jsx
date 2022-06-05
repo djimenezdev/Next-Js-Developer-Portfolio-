26da4164829e6c31742272cfca1affb06c9ed6a6
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-05 02:00:37 -0700

    repo ui updates

--- components/LatestCode/GitHubRepoCard.jsx
+++ components/LatestCode/GitHubRepoCard.jsx
@@ -1,41 +1,49 @@
-import { format } from "date-fns";
-import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { format } from 'date-fns';
+import Image from 'next/image';
 
 export const GithubRepoCard = ({ latestRepo }) => {
   return (
-    <section className="github-repo dark:bg-[#111827] bg-white shadow-shadow-white  rounded-md p-2 max-h-48">
-      <h1 className="font-semibold text-sm md:text-lg lg:text-base xl:text-lg text-white  mb-3 bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] rounded-md p-[0.5] pl-2">
+    <section className='github-repo dark:bg-[rgb(17,24,39)] bg-white shadow-shadow-white  rounded-md py-2 px-3'>
+      <h1 className='font-semibold xs:text-sm md:text-lg lg:text-base xl:text-lg text-white  mb-3 bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] rounded-md p-2'>
         {latestRepo.name.charAt(0).toUpperCase() + latestRepo.name.substring(1)}
       </h1>
-      <div className="flex space-x-4">
+      <div className='flex space-x-4'>
         <Image
           src={latestRepo.owner.avatar_url}
-          width={50}
-          height={40}
-          className="rounded-full"
-          alt="github avatar"
+          width={100}
+          height={100}
+          className='rounded-full'
+          alt='github avatar'
           quality={65}
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAaCAIAAAB6opi9AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAEySURBVFhH1VdLFsIgDAzeyrN7MjfFSaC0lJLqwMLOw8cnH4ZMFjU8X2+JEmOURYCw2DpKGiGGtMW5nQRZMK0LLC0K25DP84gp2+oAK9JYlG7hoKZk1UW+HbPmifIwrz9AyHNCnxYe0Ud6quvCoHDr06rpn0JdztzG6fIi9miTnOowkpZTSpgcaxcWU7iRtBCfxuGVs0C2/HzU1/Etn8ViBOuiJONbfjLq55EiTq3RhnInKSLi0/DZf4k2x9+IWFPjaaGaWtCmqLmKY7hdy7twG+9ntNlIWptM44IdAI7hfi3vlmEreyOAvZaEBuJe7+vUze1LRwtbAueLSHPaY5jWFBYrVCFrgvnVohtrD5JWedYpC5qZlh4/r+VddTQW/+csRQU3StGjbOfFOF/Ea2aXEPkAw5No5T4ThIoAAAAASUVORK5CYII="
+          placeholder='blur'
+          blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAaCAIAAAB6opi9AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAEySURBVFhH1VdLFsIgDAzeyrN7MjfFSaC0lJLqwMLOw8cnH4ZMFjU8X2+JEmOURYCw2DpKGiGGtMW5nQRZMK0LLC0K25DP84gp2+oAK9JYlG7hoKZk1UW+HbPmifIwrz9AyHNCnxYe0Ud6quvCoHDr06rpn0JdztzG6fIi9miTnOowkpZTSpgcaxcWU7iRtBCfxuGVs0C2/HzU1/Etn8ViBOuiJONbfjLq55EiTq3RhnInKSLi0/DZf4k2x9+IWFPjaaGaWtCmqLmKY7hdy7twG+9ntNlIWptM44IdAI7hfi3vlmEreyOAvZaEBuJe7+vUze1LRwtbAueLSHPaY5jWFBYrVCFrgvnVohtrD5JWedYpC5qZlh4/r+VddTQW/+csRQU3StGjbOfFOF/Ea2aXEPkAw5No5T4ThIoAAAAASUVORK5CYII='
         />
-        <div>
+        <div className='mt-4 text-sm md:text-lg lg:text-base xl:text-lg'>
           <p>
             By <strong>{latestRepo.owner.login}</strong>
           </p>
           <p>
-            Created on{" "}
+            Created on{' '}
             <strong>
-              {format(new Date(latestRepo.created_at), "MM-dd-yyyy")}
+              {format(new Date(latestRepo.created_at), 'MM-dd-yyyy')}
             </strong>
           </p>
         </div>
       </div>
       <a
         href={latestRepo.clone_url}
-        className="mt-3 px-2 py-4 sm:w-[40%] md:w-[67%] lg:w-[75%] w-[80%] font-semibold group flex flex-row space-x-2 items-center text-white  rounded-md text-xl bg-gradient-to-r from-[#38bdf8] to-[#3b82f6]"
+        className='my-3 px-2 py-4 w-full   font-semibold group flex flex-row space-x-2 items-center text-white  rounded-md text-xl bg-gradient-to-r from-[#38bdf8] to-[#3b82f6]'
       >
-        <p>View Repository </p>
-        <div className="transform  group-hover:translate-x-2 transition duration-300">
+        <p className='flex items-center'>
+          <FontAwesomeIcon
+            icon='fa-brands fa-github'
+            className='dark:text-white mr-3'
+            size='2x'
+          />
+          View Repository{' '}
+        </p>
+        <div className='transform  group-hover:translate-x-2 transition duration-300'>
           &rarr;
         </div>
       </a>
--- components/LatestCode/LatestCode.jsx
+++ components/LatestCode/LatestCode.jsx
@@ -1,39 +1,39 @@
-import userData from "@constants/data";
-import { GithubRepoCard } from "./GitHubRepoCard";
+import userData from '@constants/data';
+import { GithubRepoCard } from './GitHubRepoCard';
 
 export default function LatestCode({ repositories }) {
   return (
-    <section className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-40">
-      <div className="max-w-6xl mx-auto">
-        <div className="flex flex-col md:flex-row justify-between items-center md:pt-40 mx-10">
-          <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-700 my-20 md:my-0 dark:text-white text-center lg:text-left">
+    <section className='bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-10'>
+      <div className='max-w-7xl mx-auto'>
+        <div className='flex flex-col md:flex-row justify-between items-center pt-24 mx-10 md:my-12'>
+          <h1 className='text-6xl lg:text-9xl max-w-lg font-bold text-gray-700 my-10 md:my-0 dark:text-white text-center lg:text-left'>
             Latest Code
           </h1>
 
           <a
             href={`https://github.com/${userData.githubUsername}`}
-            className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center text-white"
+            className='mb-10 md:mb-0 px-8 py-4 rounded-md bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center text-white'
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-arrow-up-right-square"
-              stroke="4"
-              strokeWidth="4"
-              viewBox="0 0 16 16"
+              xmlns='http://www.w3.org/2000/svg'
+              width='16'
+              height='16'
+              fill='currentColor'
+              className='bi bi-arrow-up-right-square'
+              stroke='4'
+              strokeWidth='4'
+              viewBox='0 0 16 16'
             >
               <path
-                fillRule="evenodd"
-                d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"
+                fillRule='evenodd'
+                d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z'
               />
             </svg>
             <p>View GitHub</p>
           </a>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-10 lg:-mt-10 gap-y-20 md:pt-20">
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto px-5 lg:-mt-10 gap-y-10 md:pt-8'>
         {/* Single github Repo */}
 
         {repositories &&
