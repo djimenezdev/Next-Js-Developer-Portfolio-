6b705220bb33bddadeaf0671879607febdd499ec
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-07 15:27:01 -0700

    removed items-center class

--- components/ProjectPage/ProjectAccomplishments.tsx
+++ components/ProjectPage/ProjectAccomplishments.tsx
@@ -6,7 +6,7 @@ export const ProjectAccomplishments = ({
   accomplishments: string[];
 }) => {
   return (
-    <div className='flex-[58%] flex flex-col items-center mb-5 xl:mb-0 px-10 md:px-0'>
+    <div className='flex-[58%] flex flex-col mb-5 xl:mb-0 px-10 md:px-0'>
       <h1 className='text-3xl xs:text-4xl text-center mb-4 bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] rounded-md p-2 font-bold'>
         Accomplishments
       </h1>
