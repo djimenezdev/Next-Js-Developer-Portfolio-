ccee58be5ee4448129a6176a67cbe7676422c70b
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-14 14:24:14 -0700

    another update

--- components/AboutMe/AboutTech/AboutTech.tsx
+++ components/AboutMe/AboutTech/AboutTech.tsx
@@ -8,29 +8,26 @@ const AboutTech = ({
 }) => {
   return (
     <div className='col-span-1 md:col-span-2'>
-      {user.about.description?.map((desc, idx) => (
-        <p key={idx} className='text-xl text-gray-700 mb-4 dark:text-gray-300 '>
-          {desc}
-        </p>
-      ))}
-
       <h1 className='bg-gradient-to-r from-[#38bdf8] to-[#3b82f6] rounded-md text-3xl  px-2 py-1 md:inline-block font-bold text-gray-50'>
         Tech Stack
       </h1>
-      <div
-        className='flex flex-row items-center flex-wrap mt-8 justify-center                               
-                                          md:justify-start'
-      >
+      <div className='flex flex-row justify-center flex-wrap mt-8'>
         {Object.values(user?.techStack).map((val: string, i) =>
           val.startsWith('https') ? (
-            <div className={`mx-4 my-4 `} key={i}>
+            <div
+              className={`flex justify-center items-center mx-2 xss:mx-4 my-4 `}
+              key={i}
+            >
               <Image src={val} width={80} height={80} className='rounded-sm' />
             </div>
           ) : (
-            <div className={`mx-4 my-2`} key={i}>
+            <div
+              className={`flex justify-center items-center mx-2 xss:mx-4 my-4`}
+              key={i}
+            >
               <FontAwesomeIcon
                 icon={['fab', val]}
-                size='6x'
+                size='5x'
                 color={
                   iconColors[
                     Object.keys(user?.techStack).filter(
