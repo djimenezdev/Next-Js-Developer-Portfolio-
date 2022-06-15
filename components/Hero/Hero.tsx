cb0d2d4e7a4085355d278e192f86d04e8b40efd0
Daniel Jimenez <daniel@djimenezdev.com>
2022-06-14 18:29:02 -0700

    added desc

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import { RoughNotationGroup } from 'react-rough-notation';
 import HighlightText from './HighlightText';
 import HeroImage from './HeroImage';
+import userData from '@constants/data';
 
 const highlightSections = [
   { color: '#F59E0B', text: 'Developer.' },
@@ -14,7 +15,14 @@ export default function Hero() {
     <div className='flex flex-row md:justify-center lg:justify-between xl:justify-center items-start overflow-hidden'>
       {/* Text container */}
 
-      <div className='relative w-full md:w-[94vw] lg:w-1/2 text-center md:text-left lg:py-20 lg:pl-4 lg:pr-20 lgTwo:py-20  xl:pl-20 xl:py-20 xl:pr-24'>
+      <div className='relative w-full md:w-[94vw] lg:w-1/2 text-center md:text-left lg:pl-4 lg:pr-20   xl:pl-20  xl:pr-24'>
+        <h1 className={`text-6xl text-center  mb-2`}>{userData.name}</h1>
+        <h3 className='mb-2 text-2xl bg-gradient-to-r text-center from-[#38bdf8] to-[#3b82f6] px-2 py-1 rounded-md text-white'>
+          {userData.designation}
+        </h3>
+        <p className='text-md xss:text-xl mt-3 mb-10  text-center'>
+          "I'm a developer {userData.about.title[2]}"
+        </p>
         <RoughNotationGroup show={true}>
           {highlightSections.map(({ color, text }, i) => (
             <HighlightText color={color} text={text} key={color + '-' + i} />
